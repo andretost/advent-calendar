27467376fb6d67c94941da9416f0e3335277a6cb
Andre Tost <andretost@yahoo.com>
2025-11-03 17:43:26 -0600

    added pic to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,15 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="app-header">
-      <h1>Tareks Erste Weihnachten</h1>
-      <h2>Ein Adventskalender von Silke Tost</h2>
+      <img
+        src={`${process.env.PUBLIC_URL}/images/tarek-and-tonja.png`}
+        alt="Tarek and Tonja"
+        className="header-image"
+      />
+      <div> {/* Wrap h1 and h2 in a div */}
+        <h1>Tareks Erste Weihnachten</h1>
+        <h2>Ein Adventskalender von Silke Tost</h2>
+      </div>
     </header>
   );
 };
